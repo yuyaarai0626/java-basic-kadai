55be77fa0a0e5c8dc3d47dc739f1417aca6e1de4
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-11-27 19:38:33 +0900

    Add files via upload

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,18 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+	public static void main(String[] args) {
+		System.out.println("Aさんの点数は" + 10 + "点");
+		System.out.println("Bさんの点数は" + 20 + "点");
+		System.out.println("Cさんの点数は" + 30 + "点");
+		System.out.println("Dさんの点数は" + 40 + "点");
+		System.out.println("Eさんの点数は" + 50 + "点");
+		System.out.println("Fさんの点数は" + 60 + "点");
+		System.out.println("Gさんの点数は" + 70 + "点");
+		System.out.println("Hさんの点数は" + 80 + "点");
+		System.out.println("Iさんの点数は" + 90 + "点");
+		System.out.println("Jさんの点数は" + 100 + "点");
+		System.out.println("テストの平均点は" + ((10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10) + "点");
+	}
+
+}
