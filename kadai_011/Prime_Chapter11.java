512abf294c15108d413249bc381ab57f25bfcfc3
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-12-03 09:57:08 +0900

    Add files via upload

---
+++ kadai_011/Prime_Chapter11.java
@@ -0,0 +1,25 @@
+package kadai_011;
+
+public class Prime_Chapter11 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean[] primeJudge = new boolean[100];
+			for(int k=0; k < primeJudge.length; k++) {
+				primeJudge[k]=true;
+			}
+			for(int i = 2; i <= 100; i++) {
+				for (int r = 2; r < i; r++ ) {
+					if( i % r == 0) {
+						primeJudge[i - 1] = false;
+						break;
+					}
+				}
+			}
+			for( int j = 1; j < primeJudge.length;j++) {
+				if( primeJudge[j] == true ) {
+					System.out.println(j + 1);
+				}
+			}
+	}	
+}
