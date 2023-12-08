8e75eadd9a8d4b0c242fd8d1cf05b63f6f772f03
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-12-09 08:47:25 +0900

    Add files via upload

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -4,11 +4,11 @@ public class CarExec_Chapter14 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Car_Chapter14 gear = new Car_Chapter14(1,10);
-		Car_Chapter14 speed = new Car_Chapter14(3,30);
+		Car_Chapter14 cargear = new Car_Chapter14(1,10);
+		Car_Chapter14 carspeed = new Car_Chapter14(3,30);
 		
-		gear.gearChange(3);
-		speed.run();
+		cargear.gearChange(3);
+		carspeed.run();
 	}
 
 }
--- kadai_014/Car_Chapter14.java
+++ kadai_014/Car_Chapter14.java
@@ -2,31 +2,40 @@ package kadai_014;
 
 public class Car_Chapter14 {
     
-    private int gear;    // ギア数(1-5速)
-    private int speed; // ギアチェンジ後の速度
+    private int cargear = 1;    // ギア数(1-5速)
+    private int carspeed = 10; // ギアチェンジ後の速度
+    private int previousGear;
     
     public Car_Chapter14(int gear, int speed) {
-        this.gear = gear;
-        this.speed = speed;
+        this.cargear = gear;
+        this.carspeed = carspeed;
+    }
+    
+    public int getGear() {
+        return this.cargear;
+    }
+
+    public int getcarspeed() {
+        return this.carspeed;
     }
     
     public void gearChange(int afterGear) {
-        System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+        System.out.println("ギア" + this.cargear + "から" + afterGear + "に切り替えました");
 
         if (afterGear == 2) {
-            this.speed = 20;
+            this.carspeed = 20;
         } else if (afterGear == 3) {
-            this.speed = 30;
+            this.carspeed = 30;
         } else if (afterGear == 4) {
-            this.speed = 40;
+            this.carspeed = 40;
         } else if (afterGear == 5) {
-            this.speed = 50;
+            this.carspeed = 50;
         } else {
-            this.speed = 10;
+            this.carspeed = 10;
         }
     }
     
     public void run() {
-        System.out.println("速度は時速" + this.speed + "kmです");
+        System.out.println("速度は時速" + this.carspeed + "kmです");
     }
 }
