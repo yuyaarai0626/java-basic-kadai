1aa52592b767144e57733338c026982d1b7f7cc8
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-12-09 09:15:43 +0900

    Add files via upload

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -7,7 +7,7 @@ public class CarExec_Chapter14 {
 		Car_Chapter14 cargear = new Car_Chapter14(1,10);
 		Car_Chapter14 carspeed = new Car_Chapter14(3,30);
 		
-		cargear.gearChange(10);
+		cargear.gearChange(3);
 	}
 
 }
