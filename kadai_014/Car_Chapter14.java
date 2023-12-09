e2a1835fb51f39b2a8f7d50bd0710cafb1cc0b43
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-12-09 09:13:14 +0900

    Add files via upload

--- kadai_014/Car_Chapter14.java
+++ kadai_014/Car_Chapter14.java
@@ -1,41 +1,25 @@
 package kadai_014;
 
 public class Car_Chapter14 {
-    
-    private int cargear = 1;    // ギア数(1-5速)
-    private int carspeed = 10; // ギアチェンジ後の速度
-    private int previousGear;
-    
-    public Car_Chapter14(int gear, int speed) {
-        this.cargear = gear;
-        this.carspeed = carspeed;
-    }
-    
-    public int getGear() {
-        return this.cargear;
-    }
+	// フィールド
+	private int gear = 0;
+	private int speed = 0;
 
-    public int getcarspeed() {
-        return this.carspeed;
-    }
-    
-    public void gearChange(int afterGear) {
-        System.out.println("ギア" + this.cargear + "から" + afterGear + "に切り替えました");
+	// コントラクタ
+	public Car_Chapter14(int gear, int speed) {
+		this.gear = gear;
+		this.speed = speed;
+	}
 
-        if (afterGear == 2) {
-            this.carspeed = 20;
-        } else if (afterGear == 3) {
-            this.carspeed = 30;
-        } else if (afterGear == 4) {
-            this.carspeed = 40;
-        } else if (afterGear == 5) {
-            this.carspeed = 50;
-        } else {
-            this.carspeed = 10;
-        }
-    }
-    
-    public void run() {
-        System.out.println("速度は時速" + this.carspeed + "kmです");
-    }
+	// メソッド
+	public void gearChange(int afterGear) {
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+		int Speed = this.speed * afterGear;
+		if(Speed > 50) {
+			System.out.println("速度は時速10kmです");
+		}
+		else if(Speed <= 50) {
+			System.out.println("速度は時速" + Speed + "kmです");
+		}
+	}	
 }
