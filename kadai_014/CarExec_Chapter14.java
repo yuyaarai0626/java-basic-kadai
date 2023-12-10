057532e29c7159a0e7f712d4b2eacfe833d308f4
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-12-10 09:41:04 +0900

    Add files via upload

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -5,7 +5,6 @@ public class CarExec_Chapter14 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Car_Chapter14 cargear = new Car_Chapter14(1,10);
-		Car_Chapter14 carspeed = new Car_Chapter14(3,30);
 		
 		cargear.gearChange(3);
 	}
