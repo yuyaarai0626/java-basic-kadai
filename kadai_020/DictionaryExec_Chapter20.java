691b0db8230635fbb3245b4ac79a8a1a274e46d6
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-12-23 12:55:17 +0900

    Delete kadai_020 directory

--- kadai_020/DictionaryExec_Chapter20.java
+++
@@ -1,9 +0,0 @@
-package kadai_020;
-
-public class DictionaryExec_Chapter20 {
-	public static void main(String[] args) {
-		Dictionary_Chapter20 fruitDic = new Dictionary_Chapter20();
-		String[] words = {"apple", "banana", "grape","orange"};
-		fruitDic.seachWord(words);
-	}
-}
