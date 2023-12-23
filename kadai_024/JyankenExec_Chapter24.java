13d009834e6815d01d83eb6204cb92bf6cddfe73
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-12-23 12:54:52 +0900

    Add files via upload

---
+++ kadai_024/JyankenExec_Chapter24.java
@@ -0,0 +1,13 @@
+package kadai_024;
+
+public class JyankenExec_Chapter24 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Jyanken_Chapter24 janken = new Jyanken_Chapter24();
+		
+		janken.playGame();
+	}
+
+}
